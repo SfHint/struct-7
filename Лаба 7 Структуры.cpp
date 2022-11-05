60311037cf8955e0734df4cfcd64c1aa4c2e5851
SfHint <102695198+SfHint@users.noreply.github.com>
2022-11-05 14:24:54 +0300

    Add files via upload

---
+++ Лаба 7 Структуры.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+// Объявить структуру с заданными полями. Динамически выделить память для хранения списка. Ввести данные.
+// Выполнить задание, результат вывести на экран
+
+// Имеется список сотрудников предприятия. Каждый элемент списка содержит следующую информацию: фамилия, год рождения
+// год поступления на работу. Вывести информацию о сотрудниках фирмы, родившихся после 1985 года в порядке
+// убывания стажа работы
+
+// SELECT * FROM сотрудники WHERE год > 1985 ORDER BY стаж DESC
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	struct rabotnic
+	{
+		char fam[20];
+		int god;
+		int postup;
+	} *spisok;
+	int colvo;
+	cout << "Введите количество рабочих: " << endl;
+	cin >> colvo;
+	spisok = new rabotnic[colvo];
+	for (int i = 0; i < colvo; i++)
+	{
+		cout << "Введите фамилию: ";
+		cin >> spisok[i].fam;
+		cout << "Введите год рождения: ";
+		cin >> spisok[i].god;
+		cout << "Введите год поступления на службу: ";
+		cin >> spisok[i].postup;
+		cout << endl;
+	}
+	int k=0;
+	for (int i=0; i < colvo; i++)
+	{
+		k++;
+		if (spisok[i].god > 1985)
+		{
+
+			for(int j=0;j<k;j++)
+
+			cout << "Фамилия: " << spisok[i].fam << endl << " Год рожд: " << spisok[i].god << " Год поступ.: " << spisok[i].postup << endl;
+		}
+	}
+
+	delete[]spisok;
+	return 0;
+}
